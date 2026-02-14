d36a5991c46ce704f340a16d6650f5d3fec73be5
jammy-8 <unb1eoh2@students.codeinstitute.net>
2026-02-15 01:11:28 +0800

    Initial Commit. Django Setup

---
+++ booking/hBooking/hotelBooking/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hotelBooking(request):
+    return HttpResponse("This is the members page of the hotel booking app.")
+
+def index(request):
+    return render(request, 'hBooking/index.html')
